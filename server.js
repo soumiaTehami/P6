4dc08b91303f5381d48718dcb85aea16f9e5f2ae
tehami soumia <sou_mia_46@hotmail.fr>
2022-07-11 11:18:04 +0200

    base de donnée

--- server.js
+++ server.js
@@ -1,17 +1,42 @@
 console.log("hello")
 const express = require('express');
 const mongoose = require('mongoose');
-
+const cors = require('cors')
 const app = express();
 const port = 3000
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String
+  
+})
+const User=mongoose.model("User",userSchema)
+//middelware
+app.use(cors())
+app.use(express.json())
+//routes
+app.post('/api/auth/login',(req,res)=>{
+
+
+const email=req.body.email
+const password=req.body.password
+const user=new User({email:email,password:password})
+user
+.save()
+.then(()=> 
+  res.send({message:"user enregistré!"}))
+.catch((err)=> console.log("user pas enregistré!",err))
+ 
+})
 app.get('/', (req, res) => {
-   res.send('Hello World!')
+
+  res.send('Hello World!')
  })
  app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
+   console.log("Example app listening on port "+port)
  })
+ //database
  mongoose.connect("mongodb+srv://tehami:1988soumia@cluster0.uqmi5.mongodb.net/?retryWrites=true&w=majority", function(err) {
   if (err) { throw err;
   }
-  else { console.log("connecté base")}
+  else { console.log("Connexion à MongoDB réussie !")}
 });
